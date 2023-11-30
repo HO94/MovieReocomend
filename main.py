916525c6d79e953a18b3b4b1d71a59575d8b1e1b
H094 <lhw8187@gmail.com>
2023-11-30 21:19:38 +0900

    commit

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 # main.py
 import streamlit as st
 
-st.text('hello Streamlit!')
+st.title('Movie Recommendation')
+st.text('국민대학교 인공지능응용 5조')
+
+movie_input = st.text_input(label='Please enter the movie title')
+
+MOVIE_LIST = ['A', 'B', 'C', 'D', 'E']
+my_choice = st.selectbox('Please select a movie title', MOVIE_LIST)
